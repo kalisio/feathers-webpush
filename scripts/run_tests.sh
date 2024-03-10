4393644a594e3bece9f5f499aae30e18e620a376
tristan-greffe <tristan.greffe@hotmail.com>
2024-03-10 16:40:38 +0100

    ci: implementing new workflow using kash repository

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -46,12 +46,41 @@ yarn && yarn test
 ##
 
 install_cc_test_reporter ~
-ls -l ~/.local/bin/cc-test-reporter
-ls -l ~/cc
-~/.local/bin/cc-test-reporter format-coverage -t lcov coverage/lcov.info
-~/.local/bin/cc-test-reporter upload-coverage -r 8e87a996279373f05f01ce8166aac1bc9dda990e9a2f936af25e5aa11326b127
-
-
+send_coverage_to_cc() {
+    local CC_TEST_REPORTER_ID=$1
+    ~/.local/bin/cc-test-reporter format-coverage -t lcov coverage/lcov.info
+    ~/.local/bin/cc-test-reporter upload-coverage -r $CC_TEST_REPORTER_ID
+}
+send_coverage_to_cc 8e87a996279373f05f01ce8166aac1bc9dda990e9a2f936af25e5aa11326b127
 
 ## Notify on slack
 ##
+get_git_commit_url() {
+    local REPO_ROOT="$1"
+    local REPO_NAME="$2"
+    local COMMIT_SHA
+    COMMIT_SHA=$(get_git_commit_sha "$REPO_ROOT")
+
+    local GITHUB_REPO_URL="https://github.com/kalisio/$REPO_NAME"
+    local COMMIT_URL="$GITHUB_REPO_URL/commit/$COMMIT_SHA"
+
+    echo "$COMMIT_URL"
+}
+
+generate_slack_message() {
+    local REPO_ROOT="$1"
+    local APP="$2"
+    local WORKSPACE_BRANCH="$3"
+    local COMMIT_SHA
+    local COMMIT_URL
+    local AUTHOR
+    COMMIT_SHA=$(get_git_commit_sha "$REPO_ROOT")
+    COMMIT_URL=$(get_git_commit_url "$REPO_ROOT")
+    AUTHOR=$(get_git_commit_author_name "$THIS_DIR")
+    
+    echo "Build ([$COMMIT_SHA]($COMMIT_URL)) of $APP@$WORKSPACE_BRANCH by $AUTHOR failed"
+}
+
+message=$(generate_slack_message "$REPO_ROOT" "$APP" "$WORKSPACE_BRANCH")
+
+slack_color_log "$SLACK_WEBHOOK_LIBS" "$message" "#008000"

26d21d3a632e756d2106e6ca98bef102ccc80b61
tristan-greffe <tristan.greffe@hotmail.com>
2024-03-12 10:58:16 +0100

    ci: implementing new workflow using kash repository

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,13 +3,44 @@ set -euo pipefail
 # set -x
 
 ## Function to send slack message
-send_slack_message() {
-    # Implement your logic here to send a slack message
-    echo "Sending slack message..."
+get_git_commit_url() {
+    local ROOT_DIR="$1"
+    local APP="$2"
+    local REPO_NAME="${APP##*/}"
+    local COMMIT_SHA
+    COMMIT_SHA=$(get_git_commit_sha "$ROOT_DIR")
+    local GITHUB_REPO_URL="https://github.com/kalisio/$REPO_NAME"
+    local COMMIT_URL="$GITHUB_REPO_URL/commit/$COMMIT_SHA"
+    echo "$COMMIT_URL"
 }
 
-## Trap to handle errors
-trap send_slack_message EXIT
+get_commit_checks_url() {
+    local ROOT_DIR="$1"
+    local APP="$2"
+    local REPO_NAME="${APP##*/}"
+    local COMMIT_SHA
+    COMMIT_SHA=$(get_git_commit_sha "$ROOT_DIR")
+    echo "https://github.com/kalisio/$REPO_NAME/commit/$COMMIT_SHA/checks"
+}
+
+generate_slack_message() {
+    local ROOT_DIR="$1"
+    local APP="$2"
+    local BUILD_STATUS="$3"
+    local COMMIT_SHA
+    local COMMIT_URL
+    local COMMIT_CHECKS_URL
+    local AUTHOR
+    local BUILD_STATUS
+    COMMIT_SHA=$(get_git_commit_sha "$ROOT_DIR")
+    COMMIT_URL=$(get_git_commit_url "$ROOT_DIR" "$APP")
+    COMMIT_CHECKS_URL=$(get_commit_checks_url "$ROOT_DIR" "$APP")
+    AUTHOR=$(get_git_commit_author_name "$THIS_DIR")
+    echo "<${COMMIT_CHECKS_URL}|Build> <${COMMIT_URL}|${COMMIT_SHA}> of $APP by $AUTHOR $BUILD_STATUS"
+}
+
+
+
 
 THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
 THIS_DIR=$(dirname "$THIS_FILE")
@@ -34,7 +65,7 @@ done
 ##
 
 WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
-init_lib_infos 
+init_lib_infos "$ROOT_DIR"
 
 APP=$(get_lib_name)
 VERSION=$(get_lib_version)
@@ -42,6 +73,12 @@ VERSION=$(get_lib_version)
 echo "About to run tests for ${APP} v${VERSION} ..."
 
 . "$WORKSPACE_DIR/development/workspaces/libs/libs.sh" feathers-webpush
+
+## Notify Slack in case of failure
+##
+
+MESSAGE=$(generate_slack_message "$ROOT_DIR" "$APP" "failed")
+trap slack_color_log "$SLACK_WEBHOOK_LIBS" "$MESSAGE" "#a30200" EXIT
 
 ## Run tests
 ##
@@ -51,5 +88,14 @@ yarn && yarn test
 
 ## Publish code coverage
 ##
+if [ "$NODE_VER" -eq 16 ]; then
+    send_coverage_to_cc 8e87a996279373f05f01ce8166aac1bc9dda990e9a2f936af25e5aa11326b127
+fi
 
-send_coverage_to_cc 8e87a996279373f05f01ce8166aac1bc9dda990e9a2f936af25e5aa11326b127
+
+## Notify on slack upon successful completion
+##
+if [ "$NODE_VER" -eq 16 ]; then
+    MESSAGE=$(generate_slack_message "$ROOT_DIR" "$APP" "passed")
+    slack_color_log "$SLACK_WEBHOOK_LIBS" "$MESSAGE" "#2eb886"
+fi
